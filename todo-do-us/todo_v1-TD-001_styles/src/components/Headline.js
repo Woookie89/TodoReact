9c6bcd63cdb9518d8e025acc1d36055668311f2a
Lukasz <wojcik.l@tlen.pl>
2026-08-06 13:22:17 +0200

    fix

--- todo-do-us/todo_v1-TD-001_styles/src/components/Headline.js
+++
@@ -1,5 +0,0 @@
-function Headline() {
-    return (<h1 className='hdl'>todos</h1>);
-}
-
-export default Headline;
--- todo-do-us/todo_v1-TD-001_styles/src/utils/uuid.js
+++
@@ -1,5 +0,0 @@
-import {loadFromLocalStorage} from "./localstorage";
-
-const uuidGen = () => Math.max(...(loadFromLocalStorage('tds').map(e => e.id)), 0) + 1;
-
-export default uuidGen;
